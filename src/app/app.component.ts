c6b378ef826392fb0a003853680ac647ea83ae83
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-11-21 23:54:19 +0100

    Preloading simple implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject, AfterViewInit } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterViewInit {
+
+  constructor( @Inject(DOCUMENT) private doc: Document) { }
+
+  ngAfterViewInit() {
+    let body = this.doc.getElementsByTagName('body')[0];
+    body.removeAttribute('unresolved');
+  }
 }
